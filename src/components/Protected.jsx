876b77d1610e896dd85bcfc275136e9f5173713e
haseeb-ali-dev <h45.busy@gmail.com>
2024-01-25 15:10:23 +0500

    Setup basic component

---
+++ src/components/Protected.jsx
@@ -0,0 +1,12 @@
+/* eslint-disable react/prop-types */
+import { Navigate } from 'react-router-dom'
+
+import { getAccessToken } from '../utils/helpers'
+
+const Protected = ({ children }) => {
+    const token = getAccessToken()
+
+    return token ? children : <Navigate to='/login' />
+}
+
+export default Protected
