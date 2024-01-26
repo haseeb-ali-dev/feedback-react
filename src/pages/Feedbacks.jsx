eeee4e19a9d22a86a3768ea37452b8331bfb8645
haseeb-ali-dev <h45.busy@gmail.com>
2024-01-26 16:36:19 +0500

    Feedbacks scrollbar

--- src/pages/Feedbacks.jsx
+++ src/pages/Feedbacks.jsx
@@ -20,7 +20,24 @@ export default function Feedbacks() {
                     borderRadius: 4,
                 }}
             >
-                <List sx={{ width: '100%', bgcolor: 'background.paper', overflowY: 'auto', maxHeight: '69vh' }}>
+                <List
+                    sx={{
+                        width: '100%',
+                        bgcolor: 'background.paper',
+                        overflowY: 'auto',
+                        maxHeight: '69vh',
+                        '&::-webkit-scrollbar': {
+                            width: '0.5em',
+                        },
+                        '&::-webkit-scrollbar-track': {
+                            boxShadow: 'inset 0 0 6px rgba(0,0,0,0.00)',
+                            webkitBoxShadow: 'inset 0 0 6px rgba(0,0,0,0.00)',
+                        },
+                        '&::-webkit-scrollbar-thumb': {
+                            backgroundColor: 'rgba(0,0,0,.1)',
+                        },
+                    }}
+                >
                     {isLoading && <p>Loading...</p>}
                     {data?.feedbacks?.map((feedback, index) => (
                         <FeedbackItem key={index} row={feedback} divider={data?.feedbacks?.length - 1 !== index} />
